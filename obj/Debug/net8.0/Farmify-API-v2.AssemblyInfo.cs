2d96595af03873149f1bf58efb5861a124610e35
James Jecemeco A. Tabilog <jecemecojamestabilog@gmail.com>
2026-02-20 09:22:55 +0800

    feat: add validator

--- obj/Debug/net8.0/Farmify-API-v2.AssemblyInfo.cs
+++ obj/Debug/net8.0/Farmify-API-v2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Farmify-API-v2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+daeb1ef9866f08cdfca4845274a9ed8678c5bd21")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a066bb2907d081ba15387fb3f28cc27c407ef18")]
 [assembly: System.Reflection.AssemblyProductAttribute("Farmify-API-v2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Farmify-API-v2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
