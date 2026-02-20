51e048004adf175ecfa0ce125145cb48e5ad65f0
James Jecemeco A. Tabilog <jecemecojamestabilog@gmail.com>
2026-02-20 09:50:31 +0800

    feat: add handler for getting all users

--- src/Application/Features/Users/Handlers/GetAllUsersHandler.cs
+++ src/Application/Features/Users/Handlers/GetAllUsersHandler.cs
@@ -3,11 +3,26 @@ using Farmify_API_v2.src.Application.Features.Users.Queries;
 using Farmify_API_v2.src.Core.Interfaces.Repositories;
 using MediatR;
 
-//namespace Farmify_API_v2.src.Application.Features.Users.Handlers
-//{
-//    public class GetAllUsersHandler : IRequestHandler<GetAllUsersQuery, List<UserDto>>
-//    {
-//        private readonly IUserRepository _userRepository;
+namespace Farmify_API_v2.src.Application.Features.Users.Handlers
+{
+    public class GetAllUsersHandler : IRequestHandler<GetAllUsersQuery, List<UserDto>>
+    {
+        private readonly IUserRepository _userRepository;
 
-//    }
-//}
+        public GetAllUsersHandler(IUserRepository userRepository) => _userRepository = userRepository;
+
+        public async Task<List<UserDto>> Handle(GetAllUsersQuery request, CancellationToken ct)
+        {
+            var users = await _userRepository.GetAllAsync(ct);
+
+            return users.Select(u => new UserDto(
+                u.ID,
+                u.FirstName,
+                u.LastName,
+                u.Username,
+                u.Email,
+                u.IsActive)).ToList();
+        }
+
+    }
+}
